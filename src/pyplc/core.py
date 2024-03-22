69197d07a5bc4a182114527205b5dc453d47ad53
Linnik V.V <vlinnik@mail.ru>
2024-03-22 15:39:28 +0300

    Соединил Мердж

--- src/pyplc/core.py
+++ src/pyplc/core.py
@@ -185,7 +185,7 @@ class PYPLC():
             return False
         return True
     
-    def config(self,safe:bool=True,persist:IOBase = None,**kwds ):
+    def config(self,simulator:bool=None,persist:IOBase = None,**kwds ):
         if 'ctx' in kwds:
             ctx = kwds['ctx']
             for x in ctx:
@@ -194,7 +194,7 @@ class PYPLC():
                     var.name = x
                     self.declare( var, x )
         self.kwds = kwds
-        self.safe = safe
+        if simulator is not None: self.simulator = simulator
         if persist is not None:
             self.persist = persist
         if self.persist: #восстановление & подготовка следующей резервной копии
@@ -362,8 +362,18 @@ class PYPLC():
 
     def scan(self):
         with self:
-            for i in self.instances:
-                i( )
+            if not self.simulator:
+                i = 0
+                while i<len(self.instances):
+                    if type(self.results[i])==PYPLC.GENERATOR_TYPE:
+                        try:
+                            next(self.results[i])
+                        except StopIteration:
+                            self.results[i] = None
+                    else:
+                        self.results[i] = self.instances[i]( )
+                        if type(self.results[i])==PYPLC.GENERATOR_TYPE:i-=1
+                    i+=1
     
     def declare(self,channel: Channel, name: str = None):
         if not name:
