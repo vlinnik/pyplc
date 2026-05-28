712300384234a563c62cdbe22c4339de2c802ba1
Linnik V.V <vlinnik@mail.ru>
2026-05-28 07:43:42 +0300

    Новый NVManager
    Пересмотрен подход к сохранению-восстановлению persistent переменных.
    Теперь каждый цикл работы NVManager проверяет не изменилось ли значение
    persistent атрибута, и если да записывает. Время полного прохода/сохранения
    = кол-во переменных * цикл сканирования. При этом во время восстановления
    составляется карта использованной памяти и новые переменные размещаются по
    свободным адресам.
    Следствие 1: при добавлении NV переменной в объект старые не испортятся.
    Следствие 2: если переменную (объект) убрать из persist.json, он назначится
    на прежнее место.

--- src/pyplc/utils/nvd.py
+++ src/pyplc/utils/nvd.py
@@ -2,9 +2,200 @@ from pyplc.sfc import SFC,POU
 import json,hashlib,struct,time
 from io import IOBase
 from pyplc.utils.logging import logger
-from typing import List,Tuple,Any,Optional
+from typing import List,Tuple,Any,Optional,Union,Dict
 
-class NVD(SFC):
+LAYOUT = List[List[int]]    
+
+class NVManager(SFC):
+    info: Dict[ str,Dict[str,Union[List[str],LAYOUT]] ] = { }
+    buff = memoryview(bytearray(8))         # нужен в __read как временный буфер
+    cache: Dict[int,Any] = { }              # адрес в значение
+    
+    def __init__(self, target: IOBase, id: str = 'NVD') -> None:
+        """Программа сохраняет persistent переменные в IOBase
+
+        Args:
+            target (IOBase): Куда сохранить
+            id (str, optional): id программы. Defaults to None.
+        """
+        super().__init__(id=id)
+        self.target = target                                      #файл, куда сохраняем
+        self.timeout = 10_000_000                                 #максимум времени на сохранение
+    
+    @staticmethod
+    def sizeof(x:Any):
+        """Определение необходимого размера для сохранения
+
+        Args:
+            x (Any): Значение, которое будем сохранять
+
+        Returns:
+            int: Размер в байтах
+        """
+        t=type(x)
+        if t is bool:
+            return struct.calcsize('!b')
+        elif t is int:
+            return struct.calcsize('!q')
+        elif t is float:
+            return struct.calcsize('!d')
+        return 0
+
+    @staticmethod
+    def mkinfo(file: str = 'persist.json',persistable: List[POU]=[]): 
+        """Генерация файла информации о копии сохранения
+        
+        Информация о том где что находится в self.target хранится в специальном файле persist.json. 
+        Если появляется новая переменная - ей назначается адрес и persist.json обновляется. Если 
+        файла нет, то он создается. 
+        Формат файла - json, где по имени POU хранится список свойств,адреса,размер
+        """
+        info = { }
+        try:
+            with open(file,'r') as f:
+                info = json.load(f)
+        except OSError:
+            logger.error(f'Нет файла информации об backup(persist.json)')
+        except Exception:
+            logger.error(f'С файлом persist.json что-то не то')
+        
+        if len(persistable)==0: persistable = POU.__persistable__
+
+        if isinstance(info,list):   #было списком
+            result = { }
+            for i in info:
+                try:
+                    result[i['item']] = {'properties': i['properties'],'layout':i['layout']}
+                except:
+                    pass
+            info = result
+        NVD.info = info
+        
+        used:List[Tuple[(int,int),...]]=[]   # каждый элемент = addr,size
+        for i in info.values():
+            for l in i['layout']:
+                used.append( tuple(l) )
+        used.sort()
+        dirty = False
+                    
+        for p in persistable:
+            if p.full_id in info:
+                i = info[p.full_id]
+                if set(p._persistent_)==set(i['properties']):
+                    continue
+                logger.info(f'Изменения в {p.full_id}')
+                dirty = True
+            else:
+                info[p.full_id] = { "properties": [],"layout":[]}   #все свойства будут добавлены
+                dirty = True
+
+            item:Dict[str,List[Any]] = info[p.full_id]
+            for name in p._persistent_:
+                if name not in item['properties']:
+                    item['properties'].append(name)
+                    value = getattr(p,name)
+                    addr,size = 0,NVD.sizeof( value )
+                    for loc,req in used:
+                        if addr+size<=loc:
+                            break
+                        addr = loc+req
+                        
+                    item['layout'].append([addr,size])
+                    used.append((addr,size))
+                    used.sort()
+                    NVD.cache[addr] = value
+        if dirty:
+            try:
+                import os
+                os.rename(file,f'{file}.old')
+            except:
+                pass
+            with open(file,'w+') as f:
+                json.dump(info,f,sort_keys=True,indent='    ')
+            logger.info(f'Обновлена информация {file}. Использовано: {sum(used[-1])}')
+            
+        NVD.info = info
+
+    @staticmethod
+    def restore(source: IOBase,index: str = 'persist.json')->bool:
+        """Восстановить значение переменных из хранилища
+
+        Параметоы:
+            source: файл с поддержкой tell/seek/read
+
+        Returns:
+            bool: True если удачно False иначе
+        """        
+        if not source or len(POU.__persistable__)==0:
+            return True
+        
+        if not NVD.info: 
+            NVD.mkinfo( index, POU.__persistable__)
+            
+        for p in POU.__persistable__:
+            item = NVD.info[p.full_id]
+            for name,(off,size) in zip(item['properties'],item['layout']):
+                if size!=0:
+                    NVD.__read(source,p,name,off,size)
+            
+        return True
+
+    @staticmethod        
+    def __write(target: IOBase,value: Any, off:int, size: int):
+        target.seek(off)
+        t = type(value)
+        if t is bool:
+            struct.pack_into('!b',NVD.buff,0,value)
+        elif t is int:
+            struct.pack_into('!q',NVD.buff,0,value)
+        elif t is float:
+            struct.pack_into('!d',NVD.buff,0,value)     
+        target.write(NVD.buff[:size])
+        NVD.cache[off] = value
+    
+    @staticmethod
+    def __read(source:IOBase, so:POU,p:str,off:int,size:int):
+        t = type( getattr(so,p))
+        if off in NVD.cache:
+            value = t(NVD.cache.pop(off)) #обязательно pop: mkinfo + __write
+        else:
+            source.seek(off)
+            buff=source.read(size)
+            value = None
+            if t is bool:
+                value, = struct.unpack('!b',buff)
+            elif t is int:
+                value, = struct.unpack('!q',buff)
+            elif t is float:
+                value, = struct.unpack('!d',buff)
+            if value is not None:
+                NVD.cache[off] = t(value)
+        if value is not None:
+            setattr(so,p,t(value))
+
+    def __process(self,p: POU): #запись происходит по одной переменной
+        full_id = p.full_id
+        item = NVD.info[full_id]
+        for name,(addr,size) in zip(item['properties'],item['layout']):
+            yield
+            value = getattr(p,name)
+            if addr in NVD.cache and NVD.cache[addr]==value:
+                continue
+            NVD.__write(self.target,value,addr,size)
+        
+    def main(self):
+        """Цикл работы. Частота создания копий ограничена (не чаще 1 раз/5сек)
+        """
+
+        self.log('Запущен менеджер non-volatile переменных')
+        while True:
+            for p in self.__persistable__:
+                yield from self.__process(p)
+        
+    def __call__(self,ctx = None):
+        super().__call__()
+            
+class NVData(SFC):
     """Менеджер сохранения энергонезависимых переменных """
 
     def __init__(self, target: IOBase, id: str = 'NVD') -> None:
@@ -58,7 +249,7 @@ class NVD(SFC):
         
     @staticmethod
     def mkinfo(file: str = 'persist.json'): 
-        """Генерация файла информации об копии сохранения
+        """Генерация файла информации о копии сохранения
         """
         _,layout = NVD.__layout( )
         if not layout:
@@ -228,170 +419,5 @@ class NVD(SFC):
 
     def __call__(self,ctx = None):
         super().__call__()
-    
 
-class __NVD(POU):
-    """Менеджер сохранения энергонезависимых переменных """
-
-    def __init__(self, target: IOBase, id: str = 'NVD') -> None:
-        """Программа сохраняет persistent переменные в IOBase
-
-        Args:
-            target (IOBase): Куда сохранить
-            id (str, optional): id программы. Defaults to None.
-        """
-        super().__init__(id)
-        self.data = None
-        self.ts = time.time( )
-        self.target = target
-        self.sect_off,self.sect_n = NVD.__seek_end(target)
-        self.timeout = 1000
-    
-    @staticmethod
-    def __seek_end(target: IOBase):
-        """
-        Всего 32 секции по 8 байт. 256 байт в начале eeprom/файла. Каждая секция 
-        2 байта смещение в eeprom 
-        2 байта размер
-        4 байта порядковый номер. Порядковый номер % 32 === номер записи [0,31]
-        """
-        target.seek(0)
-        fat = target.read( 256 )
-        last = ( 256,0,0 )  # если ничего не найдем то с начала
-
-        if len(fat)<256:
-            raise RuntimeError('Persistent memory corrupted')
-        
-        off = 0
-        while off<len(fat):
-            sect_off,sect_size,sect_n = struct.unpack_from('HHI',fat,off)
-            if last[2]<=sect_n and sect_off!=0xFFFF and sect_off>=256 and sect_size<8192/2 and sect_size!=0x0 and (sect_n % 32) * 8 == off:
-                last = (sect_off,sect_size,sect_n)
-            off+=8
-        target.seek(last[0])
-        return last[0],last[2]
-
-    @staticmethod
-    def mkinfo(file: str = 'persist.json'): 
-        """Генерация файла информации об копии сохранения
-        """
-        if len(POU.__persistable__)==0: return
-        info = [ ]
-        total = 8   #заголовок записи 8 байт
-        for so in POU.__persistable__:
-            properties = sorted(so.__save__().keys())
-            sha1 = ':'.join('{:02x}'.format(x) for x in hashlib.sha1( '|'.join(properties).encode( ) ).digest( ))
-            size = len( so.to_bytearray( ) )
-            info.append( { 'item': so.full_id , 'properties': properties, 'sha1':sha1 , 'size': size  } )
-            total+=size
-
-        with open(file,'w+') as f:
-            json.dump(info,f)
-
-    @staticmethod
-    def restore(source: IOBase,index: str = 'persist.json'):
-        """Восстановить значение переменных из хранилища
-
-        Параметоы:
-            source: файл с поддержкой tell/seek/read
-
-        Returns:
-            bool: True если удачно False иначе
-        """        
-        if not source or len(POU.__persistable__)==0:
-            return False
-        
-        # NVD.__seek_end(source)
-        name = '<entry>'
-
-        try:
-            with open(index,'r') as f:
-                info = json.load(f)
-        except OSError:
-            logger.error(f'Нет файла информации об backup(persist.json)')
-            return False
-                
-        backup = POU.__persistable__    # объекты persistable
-        for i in info:  #info - список словарей, для каждого persistable объекта с указанием имени объекта, его свойств, sha1 хеша свойств и размера для сохранения
-            name = i['item']
-            size = i['size']
-            sha1 = i['sha1']
-            properties = i['properties']
-            crc = ''
-
-            try:
-                so = list( filter( lambda x: x.full_id==name, backup ) )[0]  # первый элемент из backup с именем как у текущего элемента списка
-                crc = ':'.join('{:02x}'.format(x) for x in hashlib.sha1( '|'.join(sorted(so.__save__().keys())).encode( ) ).digest( ))
-            except IndexError:
-                logger.error('Не найден #{id} в persist.json, попробуйте удалить persist.json',id=name)
-                return False
-
-            if crc != sha1:
-                raise RuntimeError(f"sha1 digest properties list is invalid: {so.id} {crc}!={sha1} {properties}")
-            
-            try:
-                data = source.read(size)
-                so.from_bytearray( data, properties )
-            except Exception as e:
-                logger.error('{e} (size={size},properties={properties})',e=e,size=size,properties=properties)
-                
-        return True
-
-    def __mkbackup(self):
-        """Сохраняет текущее состояние в буфере data
-
-        Raises:
-            Exception: Если несколько блоков имеют одинаковое full_id
-        """
-        self.data = bytearray()
-        index = []
-        for so in POU.__persistable__:
-            if so.full_id in index:
-                raise Exception(f'POU id is not unique ({so.full_id}, {index})!')
-            index.append(so.full_id)
-            self.data.extend( so.to_bytearray( ) )
-        self.data.extend(struct.pack('!q',len(self.data)))  #последнее записанное = размер backup
-        POU.__dirty__=False
-        self.ts = time.time() + 5
-        
-    def __flush(self):
-        """Сохраняет/сбрасывает внутренний буфер с ограничением по времени работы
-        """
-        if self.data is None or self.target is None:
-            return
-        done = self.target.tell() - self.sect_off   #где находимся, сколько уже сохранили
-        size = len(self.data)
-        written = 0 #сколько записали за этот вызов
-        
-        now = time.time_ns( )
-        start_ts = now
-        end_ts = start_ts + self.timeout*1000000
-
-        #запись идет постраничная, по 32 байта макс            
-        while done<size and start_ts<=now and now<end_ts:
-            npage=min(32,size-done)
-            self.target.write( self.data[done:done+npage])
-            done+=npage
-            written+=npage
-            now = time.time_ns()
-            
-        if done>=size:
-            self.data = None                #все сохранили
-            self.target.seek( (self.sect_n % 32)*8 )
-            self.target.write( struct.pack( "HHI", self.sect_off,done,self.sect_n ) )
-            self.target.flush( )
-            self.sect_off += done
-            self.sect_n += 1
-            if self.sect_off + done>=8192:
-                self.sect_off = 256
-            self.log(f'Persistent memory stat off/size/num: {self.sect_off}/{done}/{self.sect_n}')
-            self.target.seek( self.sect_off )          
-        
-    def __call__(self,ctx=None):
-        """Цикл работы. Частота создания копий ограничена (не чаще 1 раз/5сек)
-        """
-        with self:
-            if POU.__dirty__ and self.ts<time.time( ):
-                self.__mkbackup( )
-            if self.data is not None:
-                self.__flush( )
+NVD = NVManager    #старая реализация
